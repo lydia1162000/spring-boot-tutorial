7bc9d3ed9591f928f5946326d2c6834289e1fe10
Lydia Gammoh <gammohlydia94@gmail.com>
2026-02-07 16:11:55 +0300

    build end point to get course and video by id

--- src/main/java/com/qacart/spring_boot_tutorial/controllers/CourseController.java
+++ src/main/java/com/qacart/spring_boot_tutorial/controllers/CourseController.java
@@ -1,21 +1,35 @@
 package com.qacart.spring_boot_tutorial.controllers;
 
 import com.qacart.spring_boot_tutorial.entity.Course;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CourseController {
 
-    List <Course> courses = List.of(
+    List<Course> courses = List.of(
             new Course(1, "Selenium WebDriver", 30.99, 95),
             new Course(2, "Selenium WebDriver", 30.99, 95),
             new Course(3, "Selenium WebDriver", 30.99, 95)
     );
+
     @GetMapping("/courses")
     public List<Course> getCourses() {
         return courses;
+    }
+
+    @GetMapping("/course/{id}")
+    public Course getCourseById(@PathVariable int id) {
+        Optional<Course> result = courses.stream().filter(course -> course.getId() == id).findFirst();
+        if (result.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return result.get();
+
+
     }
 }
--- src/main/java/com/qacart/spring_boot_tutorial/controllers/VideoController.java
+++ src/main/java/com/qacart/spring_boot_tutorial/controllers/VideoController.java
@@ -1,10 +1,14 @@
 package com.qacart.spring_boot_tutorial.controllers;
 
 import com.qacart.spring_boot_tutorial.entity.Video;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class VideoController {
@@ -17,5 +21,12 @@ public class VideoController {
     @GetMapping("/videos")
     public List<Video> getVideos (){
         return videos;
+    }
+    @GetMapping("/video/{id}")
+    public Video getVideoById(@PathVariable int id){
+        Optional<Video> result = videos.stream().filter(video->video.getId()==id).findFirst();
+        if(result.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return result.get();
+
     }
 }
