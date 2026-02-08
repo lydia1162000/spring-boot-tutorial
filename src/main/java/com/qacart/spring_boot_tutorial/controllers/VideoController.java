22652cbbe151546d98129590fbca3d41c767fef4
Lydia Gammoh <gammohlydia94@gmail.com>
2026-02-08 13:17:35 +0300

    build the delete end point to delete course and video by id(path param)

--- src/main/java/com/qacart/spring_boot_tutorial/controllers/VideoController.java
+++ src/main/java/com/qacart/spring_boot_tutorial/controllers/VideoController.java
@@ -2,37 +2,48 @@ package com.qacart.spring_boot_tutorial.controllers;
 
 import com.qacart.spring_boot_tutorial.entity.Video;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class VideoController {
 
-    List<Video> videos = List.of(
+    List<Video> videos = new ArrayList<>(
+            List.of(
 
-            new Video(1, "hhttps:llvideo1", 20),
-            new Video(2, "hhttps:llvideo1", 20),
-            new Video(3, "hhttps:llvideo1", 20)
+                    new Video(1, "hhttps:llvideo1", 20),
+                    new Video(2, "hhttps:llvideo1", 20),
+                    new Video(3, "hhttps:llvideo1", 20)
+            )
     );
+
     @GetMapping("/videos")
-    public List<Video> getVideos (){
+    public List<Video> getVideos() {
         return videos;
     }
+
     @GetMapping("/videos/{id}")
-    public Video getVideoById(@PathVariable int id){
-        Optional<Video> result = videos.stream().filter(video->video.getId()==id).findFirst();
-        if(result.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    public Video getVideoById(@PathVariable int id) {
+        Optional<Video> result = videos.stream().filter(video -> video.getId() == id).findFirst();
+        if (result.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return result.get();
     }
-    @GetMapping("videos/search")
-    public List<Video> getVideosByURL(@RequestParam String url){
-        return videos.stream().filter(video->video.getUrl().contains(url)).toList();
+
+    @GetMapping("/videos/search")
+    public List<Video> getVideosByURL(@RequestParam String url) {
+        return videos.stream().filter(video -> video.getUrl().contains(url)).toList();
+    }
+
+    @DeleteMapping("/videos/{id}")
+    public String deleteVideoByURL(@PathVariable int id) {
+        Optional<Video> result = videos.stream().filter(video -> video.getId() == id).findFirst();
+        if (result.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        videos.remove(result.get());
+        return "Deleted";
     }
 
 }
